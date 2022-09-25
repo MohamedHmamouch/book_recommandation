41db886ec055427f78a09216c448618a30057d9f
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2022-09-26 00:12:19 +0200

    procfile and requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 
 
@@ -25,9 +26,28 @@ def recommend_ui():
 
     return render_template("recommend.html")
 
-@app.route('/recommend_books')
+@app.route('/recommend_books',methods=['post'])
 def recommend():
-    return "Hello"
+    user_input=request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:6]
+
+    data = []
+
+    for i in similar_items:
+        item = []
+
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    return render_template('recommend.html',data=data)
 
 
 if __name__ == '__main__':
